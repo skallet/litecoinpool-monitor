033e82286abd08a90375892b86cd107de4120d24
skallet <blazekm1lan@seznam.cz>
2017-09-04 21:23:16 +0200

    Added daily reporter

--- src/litecoin_monitor/core.clj
+++ src/litecoin_monitor/core.clj
@@ -18,24 +18,54 @@
     (:body)
     (parse-string)))
 
-(defn get-hashrate [pool-data]
+(defn get-param [pool-data path]
   (->
     pool-data
-    (get-in ["user" "hash_rate"])))
+    (get-in path)))
+
+(defn get-hashrate [pool-data]
+  (get-param pool-data ["user" "hash_rate"]))
+
+(defn get-expected-reward [pool-data]
+  (get-param pool-data ["user" "expected_24h_rewards"]))
+
+(defn get-past-reward [pool-data]
+  (get-param pool-data ["user" "past_24h_rewards"]))
+
 
 (defn hashrate-in-bound [real expected]
   (let [lower-bound (* (:treshold config) expected)]
     (>= real lower-bound)))
 
+(defn map-endpoints [endpoints]
+  (map
+    #(let [data (get-pool-data (:src %1))]
+      (merge %1 {:rate (get-hashrate data)
+                 :expected-reward (get-expected-reward data)
+                 :past-reward (get-past-reward data)}))
+    endpoints))
+
 (defn monitor [endpoints]
   (filter
     #(not (hashrate-in-bound (:rate %1) (:expected %1)))
-    (map
-      #(assoc %1 :rate (get-hashrate (get-pool-data (:src %1))))
-      endpoints)))
+    (map-endpoints endpoints)))
 
 (defn format-hashrate [rate]
   (str (quot rate 1000) "MH/s"))
+
+(defn create-report-message [endpoints]
+  (clojure.string/join
+    "\n"
+    (map
+      #(str
+        "Miner: *"
+        (:name %1)
+        "* got reward for past 24h _"
+        (format "%.5f" (:past-reward %1))
+        "_ and is expected _"
+        (format "%.5f" (:expected-reward %1))
+        "_")
+      endpoints)))
 
 (defn create-notify-messages [endpoints]
   (clojure.string/join
@@ -75,8 +105,18 @@
       (submit-when-message))
     (* (:ever-min config) 60 1000)))
 
+(defn start-reporter []
+  (create-job
+    #(->
+      endpoints
+      (map-endpoints)
+      (create-report-message)
+      (submit-when-message))
+    (* 24 60 60 1000)))
+
 
 (defn -main
   "I don't do a whole lot ... yet."
   [& args]
-  (start-watcher))
+  (start-watcher)
+  (start-reporter))
